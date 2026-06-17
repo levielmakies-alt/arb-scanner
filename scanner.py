c0c04d00d134bc41ca629b05204d30a6c31f2fcf
levielmakies-alt <levielmakies@gmail.com>
2026-06-17 15:43:29 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,13 +1,22 @@
 import requests
 
-kalshi = requests.get(
+r = requests.get(
     "https://api.elections.kalshi.com/trade-api/v2/events",
     timeout=20
-).json()
+)
 
-for event in kalshi["events"]:
+events = r.json()["events"]
+
+for event in events:
     title = event.get("title", "")
+
     if "pope" in title.lower():
         print(title)
         print("Ticker:", event.get("event_ticker"))
+
+        markets = event.get("markets", [])
+
+        if len(markets) > 0:
+            print("Market:", markets[0].get("ticker"))
+
         print("---")
